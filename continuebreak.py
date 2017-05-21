c7b18914e7e6d1a03beefdc80e38bb0c6da52061
vbhasker <vinodh.ab@gmail.com>
2017-05-21 07:30:53 -0500

    new control flow project

--- continuebreak.py
+++ continuebreak.py
@@ -4,3 +4,17 @@ for item in shopping_list:
     if item == 'spam':
         continue
     print('Buy {0}'.format(item))
+
+# while loop example for break
+available_exits = ['north', 'south_east']
+chosen_exit = ''
+while chosen_exit not in available_exits:
+    chosen_exit = input('Enter the exit direction: ')
+    if chosen_exit == 'quit':
+        print('Game over!')
+        break
+
+# this else is for the while. If the statement becomes true to exit the loop this gets executed.
+# doesn't get executed when break.
+else:
+    print("successfully exited!")
