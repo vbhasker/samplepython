f9c5bf2fb46a7c9b85cfb56211568a12fa74db34
vbhasker <vinodh.ab@gmail.com>
2017-09-28 21:11:48 -0500

    added regex and formatting a string.

--- chapter2.py
+++ chapter2.py
@@ -1,3 +1,53 @@
+# Regular expressions
+
+import re
+
 ingredient = 'Kumquat: 2 cups'
-print (ingredient)
+print(ingredient)
+
+pattern_text = r'(?P<ingredient>\w+):\s+' \
+               r'(?P<amount>\d+)\s+' \
+               r'(?P<unit>\w+)'
+
+pattern = re.compile(pattern_text)
+
+match = pattern.match(ingredient)
+print(match is None)
+print(match.groups())
+print(match.group('amount'))
+
+ingredient_pattern = re.compile(
+    r'(?P<ingredient>\w+):\s+'  # better way to represent the regular expression
+    r'(?P<amount>\d+)\s+'
+    r'(?P<units>\w+)'
+)
+match_new = ingredient_pattern.match(ingredient)
+print(match_new is None)
+print(match_new.groups())
+print(match_new.group('units'))
+
+
+# Formatting strings
+
+code = 'IAD'
+location = 'Dulles Intl Airport'
+max_temp = 32
+min_temp = 13
+precipitation = 0.4
+
+print('{code:3s} : {location:19s} : {max_temp:3d} / {min_temp:3d} / {precipitation:5.2f}'
+      .format(code=code, location=location,
+              max_temp=max_temp, min_temp=min_temp,
+              precipitation=precipitation))
+
+# vars stores all the variables as a dictionary
+
+print(vars())
+
+print('{code:3s} : {location:19s} : {max_temp:3d} / {min_temp:3d} / {precipitation:5.2f}'
+      .format_map(vars()))
+
+print('%3s : %19s : %3d / %3d / %5.2f' % (code, location, max_temp, min_temp, precipitation))
+
+print('{0:3s} : {1:19s} : {2:3d} / {3:3d} / {4:5.2f}'.format(code, location, max_temp, min_temp, precipitation))
 
